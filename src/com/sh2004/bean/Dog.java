21aeb12b1379932c776699a4da12ea6a6b51db67
FangJie <621001717@qq.com>
2020-11-14 18:13:51 +0800

    第一次上传test

---
+++ src/com/sh2004/bean/Dog.java
@@ -0,0 +1,17 @@
+package com.sh2004.bean;
+
+/**
+ * @ProjectName: test
+ * @Package: com.sh2004.bean
+ * @Description: java类作用描述
+ * @Author: 方杰
+ * @CreateDate: 2020/11/14 17:29
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+
+public class Dog {
+
+    private String dname;
+}
